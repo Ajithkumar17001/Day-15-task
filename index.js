d2d8ee09b388af24f4db21a3c68426553798135f
Ajith kumar <145198985+Ajithkumar17001@users.noreply.github.com>
2023-12-31 17:46:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+let count = 10;
+
+setTimeout(() => {
+    count--;
+    document.querySelector('b').innerHTML = count;
+    setTimeout(() => {
+        count--;
+        document.querySelector('b').innerHTML = count;
+        setTimeout(() => {
+            count--;
+            document.querySelector('b').innerHTML = count;
+            setTimeout(() => {
+                count--;
+                document.querySelector('b').innerHTML = count;
+                setTimeout(() => {
+                    count--;
+                    document.querySelector('b').innerHTML = count;
+                    setTimeout(() => {
+                        count--;
+                        document.querySelector('b').innerHTML = count;
+                        setTimeout(() => {
+                            count--;
+                            document.querySelector('b').innerHTML = count;
+                            setTimeout(() => {
+                                count--;
+                                document.querySelector('b').innerHTML = count;
+                                setTimeout(() => {
+                                    count--;
+                                    document.querySelector('b').innerHTML = count;
+                                    setTimeout(() => {
+                                        count = "Happy Independence Day"
+                                        document.querySelector('b').innerHTML = count;
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
